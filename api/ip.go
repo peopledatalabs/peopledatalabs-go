7a07945086825a43f9f5b8162a8b32e940a50d42
vvillait88 <vvillait88@yahoo.com>
2023-07-24 23:02:56 -0700

    add ip enrichment

---
+++ api/ip.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"context"
+
+	"github.com/peopledatalabs/peopledatalabs-go/model"
+)
+
+const (
+	ipPath = "/ip/enrich"
+)
+
+type IP struct {
+	Client
+}
+
+type IPFunc func(ctx context.Context, params model.IPParams) (model.IPResponse, error)
+
+// IP allows your users to enrich IP addresses
+func (a IP) IP(ctx context.Context, params model.IPParams) (model.IPResponse, error) {
+	if err := params.Validate(); err != nil {
+		return model.IPResponse{}, err
+	}
+	var response model.IPResponse
+	return response, a.Client.get(ctx, ipPath, params, &response)
+}
---
+++ model/ip.go
@@ -0,0 +1,97 @@
+package model
+
+import (
+	"errors"
+)
+
+type IPBaseParams struct {
+	IP string `json:"ip,omitempty" url:"ip,omitempty"` // IP that is used as the seed for enrichment
+}
+
+type IPParams struct {
+	BaseParams
+	IPBaseParams
+}
+
+func (params IPParams) Validate() error {
+	if params.IP == "" {
+		return errors.New("ip: 'IP' para must not be empty")
+	}
+	return nil
+}
+
+type IPResponse struct {
+	Status           int      `json:"status"`
+	Data             IPResult `json:"data"`
+	ReturnIPLocation bool     `json:"return_ip_location"`
+	ReturnIPMetadata bool     `json:"return_ip_metadata"`
+	ReturnPerson     bool     `json:"return_person"`
+}
+
+type IPResult struct {
+	IP      IPInfo    `json:"ip"`
+	Company IPCompany `json:"company"`
+	Person  IPPerson  `json:"person"`
+}
+
+type IPInfo struct {
+	Address  string     `json:"address"`
+	Metadata IPMetadata `json:"metadata"`
+	Location IPLocation `json:"location"`
+}
+
+type IPMetadata struct {
+	Version int  `json:"version"`
+	Mobile  bool `json:"mobile"`
+	Hosting bool `json:"hosting"`
+	Proxy   bool `json:"proxy"`
+	Tor     bool `json:"tor"`
+	VPN     bool `json:"vpn"`
+	Relay   bool `json:"relay"`
+	Service bool `json:"service"`
+}
+
+type IPLocation struct {
+	Name       string `json:"name"`
+	Locality   string `json:"locality"`
+	Region     string `json:"region"`
+	Metro      string `json:"metro"`
+	Country    string `json:"country"`
+	Continent  string `json:"continent"`
+	PostalCode string `json:"postal_code"`
+	Geo        string `json:"geo"`
+	Timezone   string `json:"timezone"`
+}
+
+type IPCompany struct {
+	Confidence      string            `json:"confidence"`
+	ID              string            `json:"id"`
+	Domain          string            `json:"domain"`
+	Name            string            `json:"name"`
+	Location        IPCompanyLocation `json:"location"`
+	Size            string            `json:"size"`
+	Industry        string            `json:"industry"`
+	InferredRevenue string            `json:"inferred_revenue"`
+	EmployeeCount   int               `json:"employee_count"`
+	Tags            []string          `json:"tags"`
+}
+
+type IPCompanyLocation struct {
+	Name          string `json:"name"`
+	Locality      string `json:"locality"`
+	Region        string `json:"region"`
+	Metro         string `json:"metro"`
+	Country       string `json:"country"`
+	Continent     string `json:"continent"`
+	StreetAddress string `json:"street_address"`
+	AddressLine2  string `json:"address_line_2"`
+	PostalCode    string `json:"postal_code"`
+	Geo           string `json:"geo"`
+}
+
+type IPPerson struct {
+	Confidence      string   `json:"confidence"`
+	JobTitleSubrole string   `json:"job_title_subrole"`
+	JobTitleRole    string   `json:"job_title_role"`
+	JobTitleLevels  []string `json:"job_title_levels"`
+}
