7499579564c6d6b84ab79f5b5a7ef7795b8a3da4
Varun Villait <vvillait88@yahoo.com>
2024-12-17 08:48:20 -0700

    add test

--- api/autocomplete_test.go
+++ api/autocomplete_test.go
@@ -27,3 +27,20 @@ func TestAutocomplete(t *testing.T) {
 	assert.Equal(t, resp.Status, http.StatusOK)
 	assert.Equal(t, resp.Data[0].Name, "stanford university")
 }
+
+func TestAutocompleteClass(t *testing.T) {
+	// setup
+	auto := Autocomplete{Client: NewClient(os.Getenv("PDL_API_KEY"), "1.0.0")}
+
+	// test
+	params := model.AutocompleteParams{
+		BaseParams:             model.BaseParams{Pretty: true, Size: 10, UpdatedTitleRoles: true},
+		AutocompleteBaseParams: model.AutocompleteBaseParams{Field: "school", Text: "sale"},
+	}
+	resp, err := auto.Autocomplete(context.Background(), params)
+
+	// assertions
+	assert.NoError(t, err)
+	assert.Equal(t, resp.Status, http.StatusOK)
+	assert.Equal(t, resp.Data[0].Name, "sales_and_marketing")
+}
